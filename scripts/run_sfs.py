39a108333af763dd0135b17f23e1d72b36444899
Ginga Kennis <gingakennis123@icloud.com>
2023-11-08 16:31:37 +0900

    update 11081631

--- scripts/run_sfs.py
+++ scripts/run_sfs.py
@@ -5,6 +5,7 @@ import yaml
 import numpy as np
 import cv2
 from src.sfs import VoxelSpace
+from src.utils import visualize_pcd
 
 YAML_FILE_PATH = "config/params.yaml"
 
@@ -16,12 +17,16 @@ def get_params():
 
 def main():
     params = get_params()
-    voxel_space = VoxelSpace(params["x_range"], params["y_range"], params["z_range"], params["voxel_size"], np.array(params["K"]),params["focal_length"])
+    voxel_space = VoxelSpace(params["x_range"], params["y_range"], params["z_range"], params["voxel_size"], np.array(params["K"]),params["focal_length"],params["table_height"])
 
     for i in range(params["num_images"]):
         image = cv2.imread(f"images/view{i+1}.png",0)
         extrinsic = np.array(params[f"extrinsic{i+1}"])
         voxel_space.sfs(image,extrinsic)
+
+        pcd, num_points = voxel_space.pointcloud
+        print(f"num_points : {num_points}")
+        visualize_pcd(pcd)
 
 
 if __name__ == "__main__":
--- src/sfs.py
+++ src/sfs.py
@@ -4,24 +4,24 @@ from src.utils import visualize_pcd
 
 
 class VoxelSpace:
-    def __init__(self, x_range, y_range, z_range, voxel_size,K,near):
+    def __init__(self, x_range, y_range, z_range, voxel_size,K,near,table_height):
         # size of each voxel
         self.voxel_size = voxel_size
 
         # number of voxel in each axis
-        self.voxel_number = [np.abs(x_range[1] - x_range[0]) / self.voxel_size[0], np.abs(y_range[1] - y_range[0]) / self.voxel_size[1], np.abs(z_range[1] - z_range[0]) / self.voxel_size[2]]
+        self.num_voxels = [np.abs(x_range[1] - x_range[0]) / self.voxel_size[0], np.abs(y_range[1] - y_range[0]) / self.voxel_size[1], np.abs(z_range[1] - z_range[0]) / self.voxel_size[2]]
         
         # total number of voxels
-        self.total_number = np.prod(self.voxel_number).astype(int)
+        self.total_number = np.prod(self.num_voxels).astype(int)
         
         # (x,y,z,projected,inside or outside silhouette,occupancy)
         self.voxels = np.ones((self.total_number, 6))
 
         l = 0
-        for z in range(1,int(self.voxel_number[2])+1):
-            for y in range(1,int(self.voxel_number[1])+1):
-                for x in range(1,int(self.voxel_number[0])+1):
-                    self.voxels[l] = [self.voxel_size[0] * (x - 0.5),self.voxel_size[1] * (y - 0.5),self.voxel_size[2] * (z - 0.5), 0.0, 0.0, 0.0] 
+        for z in range(1,int(self.num_voxels[2])+1):
+            for y in range(1,int(self.num_voxels[1])+1):
+                for x in range(1,int(self.num_voxels[0])+1):
+                    self.voxels[l] = [self.voxel_size[0] * (x - 0.5),self.voxel_size[1] * (y - 0.5),self.voxel_size[2] * (z - 0.5), 0.0, 2.0, 0.0] 
                     l += 1
         
         # (Xw,Yw,Zw,1)
@@ -31,6 +31,8 @@ class VoxelSpace:
         # camera intrinsic
         self.K = K
         self.near = near
+
+        self.table_height = table_height
         
 
     def sfs(self, image, extrinsic):
@@ -44,10 +46,11 @@ class VoxelSpace:
         points2D = np.matmul(p_matrix, self.points3D_world)
         points2D = np.floor(points2D / points2D[2, :]).astype(np.int32)
 
-        # check for points less than focal length
+        # (Xc,Yc,Zc,1)
         points3D_camera = np.matmul(extrinsic,self.points3D_world)
-        ind1 = np.where((points3D_camera[2,:] < self.near))
 
+        # check for points less than focal length
+        ind1 = np.where((points3D_camera[2,:] < self.near))
         # check for (u < 0, width < u) and (v < 0, height < v)
         ind2 = np.where(((points2D[0, :] < 0) | (points2D[0, :] >= width) | (points2D[1, :] < 0) | (points2D[1, :] >= height))) 
         points2D[:,ind2] = 0  # just for error handling
@@ -55,37 +58,34 @@ class VoxelSpace:
         # Not projected
         ind = np.unique(np.concatenate((ind1[0],ind2[0])))
 
-    
-        projected = np.ones((self.total_number, 1))
-        projected[ind,0] = 0.0
-
-
+        # 2 : not projected
         # 1 : inside silhouette
         # 0 : outside silhouette
         self.voxels[:,4] = silhouette[points2D.T[:,1], points2D.T[:,0]].astype(int)
         self.voxels[ind,4] = 2
 
-        # CASE1 (never projected() & )
-        case1 = np.where((self.voxels[:,5] == 0.0) & (self.voxels[:,3] == 0.0) & (self.voxels[:,4] == 1.0))
+        # CASE1 : never projected(self.voxels[:,3] == 0.0) & inside silhouette(self.voxels[:,4] == 1.0))
+        case1 = np.where((self.voxels[:,3] == 0.0) & (self.voxels[:,4] == 1.0))
         self.voxels[case1[0],5] = 1.0
 
-        # CASE2
-        case2 = np.where((self.voxels[:,5] == 1.0) & (self.voxels[:,4] == 0.0))
+        # CASE2 : outside silhouette(self.voxels[:,4] == 0.0)) & occupied(self.voxels[:,4] == 1.0)
+        case2 = np.where((self.voxels[:,4] == 0.0) & (self.voxels[:,5] == 1.0))
         self.voxels[case2[0],5] = 0.0
 
-        self.remove_table()
+        self.remove_table(self.table_height)
 
         # update num_projected
+        projected = np.ones((self.total_number, 1))
+        projected[ind,0] = 0.0
         self.voxels[:,3] += projected[:,0]
 
-        pcd, num_points = self.pointcloud
-        print(num_points)
-        visualize_pcd(pcd)
+        # reset inside or outside silhouette
+        self.voxels[:,4] = 2
 
             
 
-    def remove_table(self):
-        self.voxels[np.where(self.voxels[:,2] < 0.05)[0],5] = 0
+    def remove_table(self,table_height):
+        self.voxels[np.where(self.voxels[:,2] < table_height)[0],5] = 0
 
     def calc_p_matrix(self,extrinsic):
         return np.matmul(self.K,extrinsic)
@@ -107,7 +107,7 @@ class VoxelSpace:
     @property
     def voxel_space(self):
         voxel_space = self.voxels[:,5]
-        return voxel_space.reshape(int(self.voxel_number[0]),int(self.voxel_number[1]),int(self.voxel_number[2]))
+        return voxel_space.reshape(int(self.num_voxels[0]),int(self.num_voxels[1]),int(self.num_voxels[2]))
 
 
 
